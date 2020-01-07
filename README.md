036f327994cb4883b73c1a5a9657b5c3dd7bc161
Charlie <achanont@gmail.com>
2020-01-06 20:47:22 -0600

    add csv to json module

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ To install
 ```bash
 npm install csv-to-postgresql
 ```
+
+To use
+
+```bash
+const transfer = require('csv-to-postgresql');
+transfer.runSeed(obj)
+```
+
+Obj
--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-module.export.runSeed = obj => {
-  console.log('here it is', obj * 2);
+const csv = require('csvtojson');
+const Sequelize = require('sequelize');
+const pkg = require('./package.json');
+
+const testObj = { csvFilePath: './testproducts.csv', databaseName: 'testcsv' };
+
+const runSeed = obj => {
+  console.log('here it is', obj);
+  csv()
+    .fromFile(obj.csvFilePath)
+    .then(jsonObj => {
+      console.log(jsonObj);
+      //find column numbers by looking at first object
+      const length = Object.keys(jsonObj[0]).length;
+      console.log('TCL: length', length);
+      /**
+       * [
+       * 	{a:"1", b:"2", c:"3"},
+       * 	{a:"4", b:"5". c:"6"}
+       * ]
+       */
+    });
+  // const db = new Sequelize(
+  //   process.env.DATABASE_URL || `postgres://localhost:5432/${obj.databaseName}`,
+  //   {
+  //     logging: false
+  //   }
+  // );
+  // return db;
 };
+runSeed(testObj);
+
+// const execSync = require('child_process').execSync;
+// const output = execSync('ls', { encoding: 'utf-8' }); // the default is 'buffer'
+// console.log('Output was:\n', output);
