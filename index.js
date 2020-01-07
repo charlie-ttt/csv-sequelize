b5cc0a68da867c5fbc650e24d4529e2eb65fabf6
Charlie <achanont@gmail.com>
2020-01-07 10:26:54 -0600

    sequelize now works

--- index.js
+++ index.js
@@ -1,34 +1,73 @@
 const csv = require('csvtojson');
 const Sequelize = require('sequelize');
-const pkg = require('./package.json');
 
-const testObj = { csvFilePath: './testproducts.csv', databaseName: 'testcsv' };
-
-const runSeed = obj => {
-  console.log('here it is', obj);
-  csv()
-    .fromFile(obj.csvFilePath)
-    .then(jsonObj => {
-      console.log(jsonObj);
-      //find column numbers by looking at first object
-      const length = Object.keys(jsonObj[0]).length;
-      console.log('TCL: length', length);
-      /**
-       * [
-       * 	{a:"1", b:"2", c:"3"},
-       * 	{a:"4", b:"5". c:"6"}
-       * ]
-       */
-    });
-  // const db = new Sequelize(
-  //   process.env.DATABASE_URL || `postgres://localhost:5432/${obj.databaseName}`,
-  //   {
-  //     logging: false
-  //   }
-  // );
-  // return db;
+const testObj = {
+  csvFilePath: './testproducts.csv',
+  databaseName: 'testcsv',
+  tableName: 'testTable',
+  columnsInfo: [
+    'TEXT',
+    'TEXT',
+    'TEXT',
+    'TEXT',
+    'TEXT',
+    'TEXT',
+    'TEXT',
+    'TEXT',
+    'TEXT',
+    'TEXT',
+    'TEXT'
+  ]
 };
-runSeed(testObj);
+
+const seed = async obj => {
+  try {
+    // console.log('here is test data obj: ', obj);
+    const jsonObj = await csv().fromFile(obj.csvFilePath);
+    // console.log('TCL: jsonObj', jsonObj);
+
+    const db = new Sequelize(
+      process.env.DATABASE_URL ||
+        `postgres://localhost:5432/${obj.databaseName}`,
+      {
+        // logging: false
+      }
+    );
+    //forcefully drop existing tables(if any) and creates new ones. creates if they don't exist at all.
+    await db.sync({ force: true });
+    console.log('db synced!');
+
+    //initialize table by looping through CSV's json object
+    const columnNames = Object.keys(jsonObj[0]);
+    console.log('TCL: columnNames', columnNames);
+    const initializeObj = {};
+
+    // console.log('TCL: Sequelize.INTEGER', Sequelize.INTEGER);
+    columnNames.forEach(name => {
+      initializeObj[name] = {
+        type: Sequelize.INTEGER
+      };
+    });
+    initializeObj.id.primaryKey = true;
+    // delete initializeObj.id;
+    console.log('TCL: initializeObj', initializeObj);
+
+    const DBseed = db.define(obj.tableName, initializeObj);
+
+    // force: true will drop the table if it already exists
+    await DBseed.sync({ force: true });
+    // await DBseed.create({ name: 'hello' });
+
+    //closing connection
+    db.close();
+    console.log('db connection closed');
+    return db;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+seed(testObj);
 
 // const execSync = require('child_process').execSync;
 // const output = execSync('ls', { encoding: 'utf-8' }); // the default is 'buffer'
